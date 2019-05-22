f122488af005298098710afff629b30f76981483
Jake Spector <spector.jake@gmail.com>
2019-05-22 12:19:08 +0800

    Migrated and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = %w(chinese italian japanese french belgian)
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: categories.sample
+  )
+end
