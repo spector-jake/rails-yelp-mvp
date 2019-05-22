31081aff982974dca1c6284d635767d2d6290ec1
Jake Spector <spector.jake@gmail.com>
2019-05-22 16:24:33 +0800

    Adding validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,10 +5,16 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
-    redirect_to restaurant_path(@review.restaurant)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      # raise
+      render :new
+    end
   end
 
   private
