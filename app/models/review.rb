04b0349a2d1a45e63a532a00861852b5f9eda21f
Jake Spector <spector.jake@gmail.com>
2019-05-22 12:03:56 +0800

    Created Restaurant and Review models

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :restaurant, :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
